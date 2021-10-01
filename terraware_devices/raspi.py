6fcabac4ee9a843349db51b30810bed9263ad2fb
peter <peter>
2021-09-30 17:58:21 -0700

    Add missing time series

--- terraware_devices/raspi.py
+++ terraware_devices/raspi.py
@@ -26,6 +26,9 @@ class RasPiDevice(TerrawareDevice):
         # and whatnot. We'll need to figure out how to authoritatively enumerate all of this (or just change the class to only report
         # some fixed smaller set of timeseries.)
         timeseries_names = ['cpu_idle', 'cpu_iowait', 'cpu_system', 'cpu_user', 'memory_available', 'memory_total', 'temperature', 'uptime']
+        for name in ['array', 'sdcard']:
+            for counter_name in ['read_bytes', 'read_time', 'write_bytes', 'write_time', 'busy_time']:
+                timeseries_names.append(f'disk_{name}_{counter_name}')
         return [[self.id, x, 'numeric', 2] for x in timeseries_names]
 
     def poll(self):
